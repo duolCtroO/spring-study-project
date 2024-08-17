52596afac7d3cc328ca9acbc8397bb94e7602428
한영민 사원 <youngmin.han@seculayer.com>
2024-08-17 18:11:48 +0900

    Transaction 어노테이션 방식으로 수정

--- src/main/java/oort/cloud/studyproject/OrderConfig.java
+++ src/main/java/oort/cloud/studyproject/OrderConfig.java
@@ -9,11 +9,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class)
+@EnableTransactionManagement
 public class OrderConfig {
     @Bean
     public OrderRepository orderRepository(DataSource dataSource){
@@ -22,6 +24,6 @@ public class OrderConfig {
 
     @Bean
     public OrderService orderService( PlatformTransactionManager transactionManager,OrderRepository orderRepository) {
-        return new OrderServiceTxProxy(new OrderServiceImpl(orderRepository), transactionManager);
+        return new OrderServiceImpl(orderRepository);
     }
 }
